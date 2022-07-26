b48ca788b6faedefd8a04927ef0b1aa5805b70eb
Mnatsakanyan1996 <m.a.mnatsakanyan@gmail.com>
2022-07-27 00:28:43 +0400

    add .env config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import { Provider } from 'react-redux';
-import { Counter } from 'store/features/counter/Counter';
 
-import { store } from './store';
+import { store } from 'store';
+import { domainUrl } from 'configs';
+import { Counter } from 'store/features/counter/Counter';
 
 export default function App() {
   return (
     <Provider store={store}>
+      {/* tailwindcss example */}
       <h1 className="text-3xl text-center font-bold underline">
         Hello world!
+        <br />
+        {domainUrl}
       </h1>
 
-    <Counter />
+      {/* @reduxjs/toolkit example */}
+      <Counter />
 
     </Provider>
   );
---
+++ src/configs/index.js
@@ -0,0 +1,10 @@
+const {
+  REACT_APP_DOMAIN_URL,
+  REACT_APP_BASE_URL,
+} = process.env;
+
+export const domainUrl = REACT_APP_DOMAIN_URL;
+export const baseUrl = REACT_APP_BASE_URL;
+
+export const appKey = 'test';
+export const defaultLanguage = 'en';
