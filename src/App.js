46f44f0292f8015a132a6b1e351610832115a789
Mnatsakanyan1996 <m.a.mnatsakanyan@gmail.com>
2022-07-26 19:06:06 +0400

    install react-redux and @reduxjs/toolkit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Provider } from 'react-redux';
+import { Counter } from 'store/features/counter/Counter';
+
+import { store } from './store';
+
 export default function App() {
   return (
-    <h1 className="text-3xl text-center font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Provider store={store}>
+      <h1 className="text-3xl text-center font-bold underline">
+        Hello world!
+      </h1>
+
+    <Counter />
+
+    </Provider>
+  );
 }
