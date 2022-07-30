a4f902682515a0e40ee584921eb6ea40a144bcce
Mnatsakanyan1996 <m.a.mnatsakanyan@gmail.com>
2022-07-30 22:55:30 +0400

    hotfix

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -1,15 +1,28 @@
 import { createElement } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import Button from 'antd/lib/button';
 
 import MenuFoldOutlined from '@ant-design/icons/MenuFoldOutlined';
 import MenuUnfoldOutlined from '@ant-design/icons/MenuUnfoldOutlined';
 
+import { toggleState } from 'store/features/authorize/authorizeSlice';
+
 export default function AppHeader({ collapsed, setCollapsed }) {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(state => state.authorize.isLoggedIn);
+
   return (
-    <>
+    <div>
       {createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
         className: 'trigger',
         onClick: () => setCollapsed(!collapsed),
       })}
-    </>
+      
+      {/* Example */}
+      <Button onClick={() => dispatch(toggleState())}>
+        {isLoggedIn ? 'Log out' : 'Log in'}
+      </Button>
+    </div>
   );
 }
--- src/store/features/authorize/Authorize.js
+++ src/store/features/authorize/Authorize.js
@@ -1,27 +1,18 @@
-import { useSelector, useDispatch } from 'react-redux';
-
-import Button from 'antd/lib/button';
+import { useSelector } from 'react-redux';
 
 import LoginPage from 'pages/Login';
 import Loader from 'components/Loader';
-
-import { toggleState } from './authorizeSlice';
 
 export const Authorize = ({
   fallback = <LoginPage />,
   children,
 }) => {
-  const dispatch = useDispatch();
   const isLoggedIn = useSelector(state => state.authorize.isLoggedIn);
 
   const content = isLoggedIn ? children : fallback;
 
   return (
     <Loader isShow={isLoggedIn === null}>
-      {/* Example */}
-      <Button onClick={() => dispatch(toggleState())}>
-        {isLoggedIn ? 'Log out' : 'Log in'}
-      </Button>
       {content}
     </Loader>
   );
